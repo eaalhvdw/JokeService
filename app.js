51770f7a5d3c85833a86f39d619c6b2545e75eb0
eaalhvdw <louielouie5@msn.com>
2019-10-31 12:39:55 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// INITIALIZATION
+const express = require('express');
+const morgan = require('morgan');
+const config = require('./config');
+
+const app = express();
+app.use(express.static('public'));
+app.use(express.json);
+app.use(morgan('tiny'));
+
+// MONGODB & MONGOOSE SETUP
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+mongoose.connect(config.localMongoDB + '//*jokeDB*/', {useNewUrlParser: true, useUnifiedTopology: true});
+
+// ROUTES FOR THE APP
+const jokeRouter = require('./routes/joke');
+app.use('/joke', jokeRouter);
+
+// START SERVER
+const port = process.env.PORT || config.localPort;
+app.listen(port);
+console.log('Listening on port ' + port + '...');
+
+module.exports = app; // pga. tests?
