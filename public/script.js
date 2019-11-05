d7c3713912bc40e36bb35f290e0319619954c983
eaalhvdw <louielouie5@msn.com>
2019-11-05 11:15:37 +0100

    Script and jokesApi

---
+++ public/script.js
@@ -0,0 +1,49 @@
+const Handlebars = require("mongoose/lib/helpers/document/compile");
+
+setOnClicks();
+update();
+
+function update() {
+    document.querySelector('#showJokes').innerHTML = '';
+    //document.querySelector('#showOtherJokes').innerHTML = '';
+    for (let input of document.querySelectorAll('input')) input.value = '';
+
+    getJokes();
+}
+
+async function getJokes() {
+    const [template, jokeResponse] = await Promise.all([fetch('/joke.hbs')]);
+    const [templateText, jokes] = await Promise.all([template.text(), jokeResponse.json()]);
+    const compiledTemplate = Handlebars.compile(templateText);
+    let jokeHTML = '';
+    jokes.forEach(joke => {
+        jokeHTML += compiledTemplate({
+            setup: joke.setup,
+            punchline: joke.punchline
+        });
+    });
+    document.querySelector('#showJokes').innerHTML = jokeHTML;
+}
+
+function setOnClicks() {
+    document.querySelector('#submitJoke').onclick = () => {
+        const msg = {
+            setup: document.querySelector('#jokeSetup').value,
+            punchline: document.querySelector('#jokePunchline').value,
+        };
+        fetch('/joke', {
+            method: "POST",
+            body: JSON.stringify(msg),
+            headers: {'Content-Type': 'application/json'}
+        })
+            .then(response => {
+                if (response.status >= 400)
+                    throw new Error(response.status);
+                else
+                    update();
+                return response.json();
+            })
+            .then(result => console.log(`Resultat: %o`, result))
+            .catch(err => console.log('Fejl: ' + err));
+    };
+}
--- routes/jokesApi.js
+++ routes/jokesApi.js
@@ -5,6 +5,12 @@ const router = express.Router();
 router
     .get('/api/jokes', (req, res) => {
         //TODO: Vis egne jokes
+        controller.getJokes()
+            .then(val => res.json(val))
+            .catch(err => {
+                console.log(err);
+                res.status(500).send(err);
+            });
     })
     .get('/api/othersites', (req, res) => {
         //TODO: Vis liste over andre services
@@ -13,7 +19,20 @@ router
         //TODO: Vis jokes fra den valgte service
     })
     .post('api/jokes', (req, res) => {
-        //TODO: Gem ny joke
+        //TODO: Lav og gem ny joke
+        let j = undefined;
+        const {setup, punchline} = req.body;
+        controller.createJoke(setup, punchline)
+            .then(joke => {
+                j = joke;
+                return controller.getJoke(jokeId);
+            })
+            .then(() => res.json({message: 'Joke saved!'}))
+            .catch(err => {
+                console.error("Error: " + err);
+                if (err.stack) console.error(err.stack);
+                res.status(500).send(err);
+            });
     });
 
 module.exports = router;
