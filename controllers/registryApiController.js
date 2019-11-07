dbbb36e7e1fcc9ce1d1f35a8c86d76835cc7c3a4
eaalhvdw <louielouie5@msn.com>
2019-11-07 09:04:12 +0100

    Small stuff

--- controllers/registryApiController.js
+++ controllers/registryApiController.js
@@ -1,16 +1,17 @@
 "use strict";
 
+const jokeRegistryUrl = 'https://krdo-joke-registry.herokuapp.com';
+
 //Get all jokeservices from jokeRegistry
 exports.getRegistryJokes = (req, res) => {
   //TODO: GET (fetch) jokeservices from url
-
-    const jokeRegistryUrl = 'https://krdo-joke-registry.herokuapp.com';
 
 };
 
 //Register jokeservice to jokeRegistry
 exports.postJokeService = (req, res) => {
   //TODO: POST this jokeservice
+   
 };
 
 //Delete jokeservice from jokeRegistry
